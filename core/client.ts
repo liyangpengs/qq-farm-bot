ce9fb620506dcfbcdd06cbcaba6e114b8358773b
linxiaoran <xranzhuan@163.com>
2026-08-22 18:57:29 +0800

    fix(core/client): 更改自动启动账户默认值为true
    调整了worker进程启动时的autoStartAccounts配置项，将默认关闭改为默认开启，让程序默认自动启动账户

--- core/client.ts
+++ core/client.ts
@@ -46,7 +46,7 @@ if (isWorkerProcess) {
 
     runtimeEngine.start({
         startAdminServer: true,
-        autoStartAccounts: false,
+        autoStartAccounts: true,
     }).catch((err: any) => {
         mainLogger.error('runtime bootstrap failed', { error: err && err.message ? err.message : String(err) });
     });
